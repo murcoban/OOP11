152842ece45395dadf986089a7b67b1cc8b5fa20
MURAT ÇOBAN <murcoban@gmail.com>
2021-01-23 11:03:29 +0300

    Proje dosyası ekle.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        // class new olunca MyList otomatik çalışır
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+          
+            for (int i = 0; i < tempArray.Length ; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[tempArray.Length-1] = item;
+        }
+    }
+}
