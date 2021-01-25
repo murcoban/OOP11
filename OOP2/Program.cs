c6a3433e1abc62046da30edb77f3352794911a77
MURAT ÇOBAN <murcoban@gmail.com>
2021-01-25 09:07:11 +0300

    Yeni

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -6,7 +6,13 @@ namespace OOP2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.Id = 1;
+            musteri1.TcNo = "12345678912";
+            musteri1.MusteriNo = "12345";
+            musteri1.SirketAdi = "?";
         }
     }
 }
